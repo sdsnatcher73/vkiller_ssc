a92353d82b72cc315c615d815be17c1fab1bd134
Jan van Valburg <jan.van.valburg@activision.com>
2017-10-29 19:58:36 +0000

    Misc fixes
    Improved RAM mapping function so that it selects the subslot correctly
    Shared RAM mapping function to save memory
    Set a flag so that the game-over screen doesn't appear before the music has stopped
    Correct handling of various commands e.g. pause

--- vkiller_scc.asm
+++ vkiller_scc.asm
@@ -81,19 +81,17 @@ not_register_seven:
         pop     af
         ret
 
-
-music_start:
-        di
-        ; swap RAM into 0000-3fff
+map_slots:
+        ; map RAM into bank 0000-3fff
         ld      c, 0a8h
         in      b, (c)
+        push    bc
+        push    hl
         ld      a, b
         or      03h
         out     (c), a
 
         ; get subslot from page 3 (c000-ffff) and apply to page 0 (0000-3fff)
-        push    bc
-        push    hl
         ld      hl, 0ffffh
         ld      a, (hl)
         cpl
@@ -108,15 +106,28 @@ music_start:
         or      c
         ld      (hl), a
 
-        ; map music-player ROM pages into 8000-bfff
+        ; map music data using konami SCC mapper
         ld      a, 17
         ld      (09000h), a
         ld      a, 18
         ld      (0b000h), a
+        pop     af
+        pop     hl
+        pop     bc
+        ret
+
+music_start:
+        di
+
+        call    map_slots
+
+        push    bc
+        push    hl
+        push    af
 
         ; interpret the command
         ld      a, e
-        cp      0xfc  ; 0xfc and higher = commands: fade out, pause, etc.
+        cp      0xfa  ; 0xfa and higher = commands: fade out, pause, etc.
         jr      nc, music_start_command
         or      a
         jr      z, music_stop
@@ -157,7 +168,7 @@ music_stop:
         jr      music_start_call
 
 music_start_command:
-        and     03h
+        and     07h
         ld      d, 0
         ld      e, a
         ld      hl, command_convert_table
@@ -167,8 +178,6 @@ music_start_command:
 music_start_call:
         push    bc
         call    06003h  ; nemesis 3 song start function
-        ;call    06180h  ; play endlessly (without this the music will stop after a while)
-        ; = command 0x83 - doesn't seem to be needed anymore(?)
         pop     bc
 
 music_start_skip:
@@ -199,38 +208,19 @@ music_update:
         push    ix
         push    iy
 
-        ; map RAM into bank 0000-3fff
-        ld      c, 0a8h
-        in      b, (c)
+        call    map_slots
+
         push    bc
-        ld      a, b
-        or      03h
-        out     (c), a
-
-        ; get subslot from page 3 (c000-ffff) and apply to page 0 (0000-3fff)
-        ld      hl, 0ffffh
-        ld      a, (hl)
-        cpl
         push    af
-        ld      b, a
-        and     011000000b
-        rlca    
-        rlca
-        ld      c, a
-        ld      a, b
-        and     011111100b
-        or      c
-        ld      (hl), a
-
-        ld      a, 17
-        ld      (09000h), a
-        ld      a, 18
-        ld      (0b000h), a
 
         call    06006h  ; nemesis 3 song update function
 
         pop     af
         ld      (0ffffh), a
+
+        ; set a flag which vkiller uses to know whether music is playing
+        ld      a,(020c0h)
+        ld      (0c0a7h), a
 
         ; restore ROM
         pop     bc
@@ -250,9 +240,13 @@ ram_not_initialised:
         ret
 
 command_convert_table:
-        db      081h  ; 0fch = ?
-        db      082h  ; 0fdh = pause
-        db      081h  ; 0feh = unpause
+        db      0     ; 0f8h = ?
+        db      0     ; 0f9h = ?
+        db      0     ; 0fah = ?
+        db      082h  ; 0fbh = pause (hourglass0
+        db      081h  ; 0fch = unpause (hourglass)
+        db      082h  ; 0fdh = pause (F1)
+        db      081h  ; 0feh = unpause (F1)
         db      084h  ; 0ffh = fade out
 
 end_of_program:
